5e96229b74728efa31853ff37c1f78769957d445
balachandra.d <chandradasari400@gmail.com>
2025-10-20 23:16:42 +0530

    initial commit

---
+++ app/src/main/java/com/example/cleanarchitecturenotesapp/feauturenote/domain/model/Note.kt
@@ -0,0 +1,22 @@
+package com.example.cleanarchitecturenotesapp.feauturenote.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.cleanarchitecturenotesapp.ui.theme.Pink40
+import com.example.cleanarchitecturenotesapp.ui.theme.Pink80
+import com.example.cleanarchitecturenotesapp.ui.theme.Purple40
+import com.example.cleanarchitecturenotesapp.ui.theme.Purple80
+import com.example.cleanarchitecturenotesapp.ui.theme.PurpleGrey40
+
+@Entity
+data class Note(
+    val title :String,
+    val content : String,
+    val timeStamp:Long,
+    val color : Int,
+    @PrimaryKey val id :Int? =null
+    ){
+  companion object{
+      val noteColors = listOf(Purple80,Purple40,PurpleGrey40,Pink80,Pink40)
+  }
+}
